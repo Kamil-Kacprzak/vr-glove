28147981ee7f66b132dcf8e38ae5c47bb4c9274d
Kamil Kacprzak <Kamil.Kacprzak@yahoo.com>
2020-01-26 12:44:35 +0100

    Created data tab design full

---
+++ VrGlove_UI/app/src/main/java/com/example/vrglove/ui/main/PlaceholderFragment.java
@@ -0,0 +1,70 @@
+package com.example.vrglove.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.vrglove.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root ;
+
+        switch (getArguments().getInt(ARG_SECTION_NUMBER))
+        {
+            case 1:
+                    root = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+            case 2:
+                 root = inflater.inflate(R.layout.fragment_open_gl, container, false);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+               // textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
